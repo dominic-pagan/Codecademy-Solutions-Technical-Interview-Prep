1b4bde95bc6d082fcd7513cdea1c20ec439441c6
Dominic Pagan <45722322+dominic-pagan@users.noreply.github.com>
2020-07-10 09:55:44 -0700

    Create strings.py

---
+++ Learn Python 2/2. Strings and Console Output/strings.py
@@ -0,0 +1,19 @@
+"""STRINGS & CONSOLE OUTPUT
+Strings
+Another useful data type is the string. A string can contain letters, numbers, and symbols.
+
+name = "Ryan"
+age = "19"
+food = "cheese"
+In the above example, we create a variable name and set it to the string value "Ryan".
+We also set age to "19" and food to "cheese".
+Strings need to be within quotes.
+
+Instructions
+1.
+Create a new variable brian and assign it the string "Hello life!".
+"""
+
+# Set the variable brian on line 3!
+
+brian = "Hello life!"
